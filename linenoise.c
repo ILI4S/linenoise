d27d72e053ca35600edb87d1b47f7c3dda4fe07c
ILI4S <ilias.karim@gmail.com>
2017-06-24 16:13:04 -0400

    Rename ENTER ENTER_KEY

--- linenoise.c
+++ linenoise.c
@@ -162,7 +162,7 @@ enum KEY_ACTION{
 	TAB = 9,            /* Tab */
 	CTRL_K = 11,        /* Ctrl+k */
 	CTRL_L = 12,        /* Ctrl+l */
-	ENTER = 13,         /* Enter */
+	ENTER_KEY = 13,     /* Enter */
 	CTRL_N = 14,        /* Ctrl-n */
 	CTRL_P = 16,        /* Ctrl-p */
 	CTRL_T = 20,        /* Ctrl-t */
@@ -812,7 +812,7 @@ static int linenoiseEdit(int stdin_fd, int stdout_fd, char *buf, size_t buflen, 
         }
 
         switch(c) {
-        case ENTER:    /* enter */
+        case ENTER_KEY:    /* enter */
             history_len--;
             free(history[history_len]);
             if (mlmode) linenoiseEditMoveEnd(&l);
